ae9e47b0612cda8ff5df7968f0a1acde843af2a5
De La Faire Geoffroy <geoffroy.de-la-faire@student-cs.fr>
2026-06-02 11:20:11 +0200

    feature_correlation 1st try

--- Higgs_collaborations/sample_code_submission/feature_analysis.py
+++ Higgs_collaborations/sample_code_submission/feature_analysis.py
@@ -1,5 +1,27 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+    
+
 def feature_corrilations(data):
-    pass
+    
+    
+    #we Handle the -25 sentinel value#
+    data_cleaned = data.replace(-25, np.nan)
+    
+    #we create vectors with the physical values, we don't take the colums labels"
+    feature_cols = [col for col in data.columns if col.startswith('PRI_') or col.startswith('DER_')]
+    
+    #calcul of the matrix of correlation#
+    corr_matrix = data_cleaned[feature_cols].corr()
+    
+    print(f"=== Correlation matrix ===")
+    print(f"Dimensions of the matrix : {corr_matrix.shape} (28x28)")
+    
+    print(corr_matrix)
+    return corr_matrix
+    
 
 
 def systematics_dependence(data):
