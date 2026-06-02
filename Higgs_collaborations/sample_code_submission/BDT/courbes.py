50176c92a6c51988ef658ccb350623ffc7345720
Maxime Nègre <maxime.negre@student-cs.fr>
2026-06-02 14:08:26 +0200

    Ajout des fonctions pour calculer la signification maximale, le tracé des 3 méthodes arrivent après

--- Higgs_collaborations/sample_code_submission/BDT/courbes.py
+++ Higgs_collaborations/sample_code_submission/BDT/courbes.py
@@ -44,3 +44,45 @@ data = pd.DataFrame(
         "lep_phi_1",
     ],
 )
+
+#Calcul de la signification maximale
+
+def amsasimov(s_in, b_in):
+    """Calcule la signification d'Asimov (arXiv:1007.1727 eq. 97)"""
+    s = np.copy(s_in)
+    b = np.copy(b_in)
+    s = np.where((b_in == 0), 0.0, s_in)
+    b = np.where((b_in == 0), 1.0, b)
+
+    ams = np.sqrt(2 * ((s + b) * np.log(1 + s / b) - s))
+    ams = np.where((s < 0) | (b < 0), np.nan, ams)
+    if np.isscalar(s_in):
+        return float(ams)
+    else:
+        return ams
+
+
+def significance_vscore(y_true, y_score, sample_weight=None):
+    """Calcule le vecteur de signification Z selon le seuil de coupure"""
+    if sample_weight is None:
+        sample_weight = np.full(len(y_true), 1.0)
+
+    bins = np.linspace(0, 1.0, 101)
+    s_hist, _ = np.histogram(
+        y_score[y_true == 1], bins=bins, weights=sample_weight[y_true == 1]
+    )
+    b_hist, _ = np.histogram(
+        y_score[y_true == 0], bins=bins, weights=sample_weight[y_true == 0]
+    )
+
+    # Sommes cumulées inversées (du plus haut score au plus bas)
+    s_cumul = np.cumsum(s_hist[::-1])[::-1]
+    b_cumul = np.cumsum(b_hist[::-1])[::-1]
+
+    return amsasimov(s_cumul, b_cumul)
+
+
+def significance_score(y_true, y_score, sample_weight=None):
+    """Retourne la signification maximale trouvée (Z max)"""
+    vsig = significance_vscore(y_true, y_score, sample_weight)
+    return np.max(vsig)
