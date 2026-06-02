30906ace7bba511f9efe405a2becbe69e7bf5344
Kiruan <kiruan.perezvaras@student-cs.fr>
2026-06-02 14:58:29 +0200

    met fitter et bkg fitter functions kiruan

--- Higgs_collaborations/sample_code_submission/systematic_analysis.py
+++ Higgs_collaborations/sample_code_submission/systematic_analysis.py
@@ -62,3 +62,79 @@ def jes_fitter(
         return array * jes
 
     return fit_function
+
+def met_fitter(
+    model,
+    eval_set,
+    met_variations=np.array([-3.0, 0.0, 3.0])
+):
+    """
+    Analyse de l'incertitude Soft MET.
+    On applique des variations absolues (en GeV).
+    """
+    histograms = []
+    bins = 100
+    
+    # 1. On génère les histogrammes pour chaque variation de MET
+    for met in met_variations:
+        # /!\ Vérifie le nom de l'argument: soft_met ou met_unclustered
+        syst_set = systematics(eval_set, soft_met=met) 
+        score = model.predict(syst_set["data"])
+        
+        hist, _ = np.histogram(score, bins=bins, range=(0, 1))
+        histograms.append(hist)
+        
+    histograms = np.array(histograms)
+    
+    # 2. On "fit" un polynôme de degré 2 pour chaque bin de l'histogramme
+    poly_coeffs = []
+    for i in range(bins):
+        coeffs = np.polyfit(met_variations, histograms[:, i], deg=2)
+        poly_coeffs.append(coeffs)
+        
+    poly_coeffs = np.array(poly_coeffs)
+    
+    # 3. La fonction à renvoyer à stat
+    def fit_function(base_histogram, met_value):
+        new_hist = np.zeros_like(base_histogram, dtype=float)
+        for i in range(len(new_hist)):
+            new_hist[i] = np.polyval(poly_coeffs[i], met_value)
+        return np.clip(new_hist, 0, None) # Empêche d'avoir un nombre de particules négatif
+
+    return fit_function
+
+
+
+    
+def bkg_fitter(model, eval_set, bkg_variations=np.array([0.95, 1.00, 1.05])):
+    """
+    Analyse de l'incertitude sur la normalisation du bruit de fond (BKGnorm).
+    Variation relative (0.95 = -5%, 1.05 = +5%).
+    """
+    histograms = []
+    bins = 100
+    
+    for bkg in bkg_variations:
+        # /!\ Vérifie le nom de l'argument: bkg_scale, bkg_norm, etc.
+        syst_set = systematics(eval_set, bkg_scale=bkg)
+        score = model.predict(syst_set["data"])
+        
+        hist, _ = np.histogram(score, bins=bins, range=(0, 1))
+        histograms.append(hist)
+        
+    histograms = np.array(histograms)
+    
+    poly_coeffs = []
+    for i in range(bins):
+        coeffs = np.polyfit(bkg_variations, histograms[:, i], deg=2)
+        poly_coeffs.append(coeffs)
+        
+    poly_coeffs = np.array(poly_coeffs)
+    
+    def fit_function(base_histogram, bkg_value):
+        new_hist = np.zeros_like(base_histogram, dtype=float)
+        for i in range(len(new_hist)):
+            new_hist[i] = np.polyval(poly_coeffs[i], bkg_value)
+        return np.clip(new_hist, 0, None)
+
+    return fit_function
