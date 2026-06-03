ac4979e73f8dd58902c5e591d4ae279ced93522c
kdaniel351 <hyeongwon.kim@student-cs.fr>
2026-06-03 13:50:38 +0200

    Update statistical_analysis.py
    Coquilles

--- Higgs_collaborations/sample_code_submission/statistical_analysis.py
+++ Higgs_collaborations/sample_code_submission/statistical_analysis.py
@@ -26,11 +26,9 @@ Task 2 : Systematic Uncertainty
 4. return the mu and its uncertainty
 
 """
-
-
 def compute_mu(score, weight, saved_info):
 
-    score = score.flatten() > 0.5 
+    score = score.flatten() > saved_info["threshold"]
     score = score.astype(int)
 
     N_obs = np.sum(score * weight)
@@ -51,35 +49,6 @@ def compute_mu(score, weight, saved_info):
         "del_mu_sys": del_mu_sys,
         "del_mu_tot": del_mu_tot,
     }
-    
-def compute_mu_unbinned_likelihood(mu0, S_scores, S_weights, B_scores, B_weights, Data_scores, Data_weights):
-    '''
-    Estimate mu by minimizing the NLL using Minuit and unbinned data
-    '''
-    pdf_S, pdf_B, N_S, N_B = prepare_unbinned(S_scores, S_weights, B_scores, B_weights) 
-    nll_func = lambda mu: unbinned_NLL(mu, Data_scores, Data_weights, pdf_S, pdf_B, N_S, N_B)
-    m = Minuit(nll_func, mu=1.0)
-    m.limits["mu"] = (0, None)
-    m.errordef = Minuit.LIKELIHOOD
-    m.migrad() 
-    m.hesse()  
-    print("mu_hat =", m.values["mu"])
-    print("sigma_mu =", m.errors["mu"])
-    print("NLL_min =", m.fval)
-
-def compute_mu_binned_likelihood(mu0, N_bins, S_scores, S_weights, B_scores, B_weights, Data_scores, Data_weights):
-    '''
-    Estimate mu by minimizing the NLL using Minuit and bins
-    '''
-    Nb, Sb, Bb = prepare_binned(N_bins, S_scores, S_weights, B_scores, B_weights, Data_scores, Data_weights)
-    m = Minuit(lambda mu: NLL(mu, Nb, Sb, Bb), mu=mu0) #mu est le paramètre à estimer, initialisé à mu0 
-    m.errordef = Minuit.LIKELIHOOD # on minimise NLL
-    m.migrad()  # recherche du minimum
-    m.hesse()   # calcul des erreurs
-    print("mu_hat =", m.values["mu"])  #valeur estimée de mu qui minimise la NLL
-    print("sigma_mu =", m.errors["mu"]) #incertitudes sur mu
-    print("NLL_min =", m.fval) # valeur minimale de NLL3
-    
 
 def calculate_saved_info(model, holdout_set):
 
@@ -88,7 +57,7 @@ def calculate_saved_info(model, holdout_set):
     print("score shape before threshold", score.shape)
 
     #We compute the optimized threshold by avergage median signficance method
-    MAX = sorted(set(score))[-1] 
+    MAX = sorted(set(score.flatten()))[-1] 
     threshold_list = np.linspace(0, MAX, 100) #We generate 100 values of potential threshold values 
     ams = [0] * 100
  
@@ -97,17 +66,19 @@ def calculate_saved_info(model, holdout_set):
         score_bis = score.flatten() > t
         score_bis = score_bis.astype(int)
  
+        if np.sum(score_bis)==0: 
+            continue
+
         label = holdout_set["labels"]
  
-        gamma = np.sum(holdout_set["weights"] * score2 * label)
+        gamma = np.sum(holdout_set["weights"] * score_bis * label)
  
-        beta = np.sum(holdout_set["weights"] * score2 * (1 - label))
+        beta = np.sum(holdout_set["weights"] * score_bis* (1 - label))
  
         ams[i] = np.sqrt(2 * ((gamma + beta) * np.log(1 + gamma / beta) - gamma))
  
     index = np.argmax(ams)
-    threshold = threshold_list[best_idx]   #We keep the threshold value that maximizes the AMS
-
+    threshold = threshold_list[index]
     score = score.flatten() > threshold
     score = score.astype(int)
 
@@ -133,7 +104,7 @@ def calculate_saved_info(model, holdout_set):
     "threshold": threshold
     }
 
-    print("saved_info", saved_info)
+    print("saved_info", saved_info))
 
 N_bins = 5
 
